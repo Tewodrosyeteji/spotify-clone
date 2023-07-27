1494f2f9b19ed8331eaef5a79469f3909249d8f4
Tewodrosyeteji <teddytejinesh@gmail.com>
2023-07-27 16:05:18 +0300

    useUser-hook

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import Sidebar from "@/components/Sidebar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
+import { SupabaseClient } from "@supabase/auth-helpers-nextjs";
+import SupabaseProvider from "@/providers/SupabaseProvider";
 
 const figtree = Figtree({ subsets: ["latin"] });
 
@@ -18,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={figtree.className}>
-        <Sidebar>{children}</Sidebar>
+        <SupabaseProvider>
+          <Sidebar>{children}</Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
   );
